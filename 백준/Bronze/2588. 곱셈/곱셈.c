f4dc78a25e304d4c7b39f8d8b9c6ada06d34c0ee
seonghunppark <sunghun0483@gmail.com>
2025-07-25 09:05:55 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+	int A;
+	int B;
+
+	printf("");
+	scanf("%d%d", &A, &B);
+	
+
+	printf("%d\n", (A * (B % 10)));
+	printf("%d\n", (A * ((B % 100) / 10)));
+	printf("%d\n", (A * (B / 100)));
+	printf("%d\n", (A * B));
+
+
+
+	return 0;
+}
