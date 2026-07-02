ab0eb1de4bdf05b78008c39b0c90c9bce725b540
seonghunppark <sunghun0483@gmail.com>
2026-07-02 22:54:55 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 1.89 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    int c;
+    cin >> a >> b;
+    c = a + b;
+	printf("%d + %d = %d", a, b, c); 
+    return 0;
+}
